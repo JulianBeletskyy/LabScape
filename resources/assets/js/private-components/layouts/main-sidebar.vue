871dd816ca729fa98066c2baaa95e6ff5f9596a0
sergezak <sergezak9@gmail.com>
2018-04-11 21:01:05 +0300

    implemented tab filters in main sidebar #4

--- resources/assets/js/private-components/layouts/main-sidebar.vue
+++ resources/assets/js/private-components/layouts/main-sidebar.vue
@@ -4,35 +4,64 @@
         <section class="sidebar">
 
             <div class="sidebar-form">
-                <div class="form-group">
-                    <select class="form-control select-filter type-filter">
-                        <option>Type</option>
-                        <option>option 2</option>
-                        <option>option 3</option>
-                        <option>option 4</option>
-                        <option>option 5</option>
-                    </select>
-                    <select class="form-control select-filter used-products-filter">
-                        <option>Used Products</option>
-                        <option>Tags</option>
-                        <option>option 3</option>
-                        <option>option 4</option>
-                        <option>option 5</option>
-                    </select>
-                    <select class="form-control select-filter tags-filter">
-                        <option>Tags</option>
-                        <option>option 2</option>
-                        <option>option 3</option>
-                        <option>option 4</option>
-                        <option>option 5</option>
-                    </select>
-                    <select class="form-control select-filter sort-by-filter">
-                        <option>Sort By</option>
-                        <option>option 2</option>
-                        <option>option 3</option>
-                        <option>option 4</option>
-                        <option>option 5</option>
-                    </select>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <select class="form-control select-filter type-filter">
+                                <option>Type</option>
+                                <option>option 2</option>
+                                <option>option 3</option>
+                                <option>option 4</option>
+                                <option>option 5</option>
+                            </select>
+                            <select class="form-control select-filter used-products-filter">
+                                <option>Used Products</option>
+                                <option>Tags</option>
+                                <option>option 3</option>
+                                <option>option 4</option>
+                                <option>option 5</option>
+                            </select>
+                            <select class="form-control select-filter tags-filter">
+                                <option>Tags</option>
+                                <option>option 2</option>
+                                <option>option 3</option>
+                                <option>option 4</option>
+                                <option>option 5</option>
+                            </select>
+                            <select class="form-control select-filter sort-by-filter">
+                                <option>Sort By</option>
+                                <option>option 2</option>
+                                <option>option 3</option>
+                                <option>option 4</option>
+                                <option>option 5</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="form-group margin-bottom-0">
+                            <ul class="tab-filter">
+                                <li>
+                                    <a href="javascript:void(0)" class="active">
+                                        All Labs</a>
+                                </li>
+                                <li class="my-customers">
+                                    <a href="javascript:void(0)">
+                                        <span class="oval"></span>
+                                        My customers
+                                    </a>
+                                </li>
+                                <li class="potential-customers">
+                                    <a href="javascript:void(0)">
+                                        <span class="oval"></span>
+                                        Potential Customers
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
             <!-- /.search form -->
