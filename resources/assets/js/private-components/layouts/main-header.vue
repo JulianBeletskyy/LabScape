8564a3dbeaf9232e389cc2e2c6e2ac17a9521c0a
sergezak <sergezak9@gmail.com>
2018-04-11 19:57:52 +0300

    re-ordered toogle and logo in header #4

--- resources/assets/js/private-components/layouts/main-header.vue
+++ resources/assets/js/private-components/layouts/main-header.vue
@@ -1,18 +1,19 @@
 <template>
     <header class="main-header">
         <!-- Logo -->
-        <router-link to="/" class="logo">
+
             <!-- mini logo for sidebar mini 50x50 pixels -->
-            <span class="logo-mini"><b>A</b>LT</span>
-            <!-- logo for regular state and mobile devices -->
-            <span class="logo-lg"><img src="/images/logo.png" alt=""></span>
-        </router-link>
-        <!-- Header Navbar: style can be found in header.less -->
+        <a href="#" class="sidebar-toggle new-toogle" data-toggle="push-menu" role="button">
+            <span class="sr-only">Toggle navigation</span>
+        </a>
+
+
+
         <nav class="navbar navbar-static-top">
-            <!-- Sidebar toggle button-->
-            <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
-                <span class="sr-only">Toggle navigation</span>
-            </a>
+
+            <div class="header-logo">
+                <span class="logo-lg"><img src="/images/logo.png" alt=""></span>
+            </div>
 
             <div class="nav-search">
                 <ul class="nav navbar-nav">
