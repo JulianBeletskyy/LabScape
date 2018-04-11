6381b3db054131ace78c99a728310c09fe881cf1
sergezak <sergezak9@gmail.com>
2018-04-11 19:44:20 +0300

    added search field in the header

--- resources/assets/js/private-components/layouts/main-header.vue
+++ resources/assets/js/private-components/layouts/main-header.vue
@@ -1,12 +1,12 @@
 <template>
     <header class="main-header">
         <!-- Logo -->
-        <a href="index2.html" class="logo">
+        <router-link to="/" class="logo">
             <!-- mini logo for sidebar mini 50x50 pixels -->
             <span class="logo-mini"><b>A</b>LT</span>
             <!-- logo for regular state and mobile devices -->
             <span class="logo-lg"><img src="/images/logo.png" alt=""></span>
-        </a>
+        </router-link>
         <!-- Header Navbar: style can be found in header.less -->
         <nav class="navbar navbar-static-top">
             <!-- Sidebar toggle button-->
@@ -14,37 +14,15 @@
                 <span class="sr-only">Toggle navigation</span>
             </a>
 
-            <div class="navbar-custom-menu">
+            <div class="nav-search">
                 <ul class="nav navbar-nav">
-                    <!-- User Account: style can be found in dropdown.less -->
-                    <li class="dropdown user user-menu">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            <img :src="user.avatar" class="user-image" alt="User Image">
-                            <span class="hidden-xs"> {{user.surname}} {{user.name}}</span>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <!-- User image -->
-                            <li class="user-header">
-                                <img :src="user.avatar" class="img-circle" alt="User Image">
-
-                                <p>
-                                    {{user.surname}} {{user.name}}
-                                    <!--<small>Member since </small>-->
-                                </p>
-                            </li>
-                            <!-- Menu Footer-->
-                            <li class="user-footer">
-                                <div class="pull-left">
-                                    <router-link to="/user/profile" class="btn btn-default btn-flat">Profile</router-link>
-                                </div>
-                                <div class="pull-right">
-                                    <a  class="btn btn-default btn-flat" @click="doLogOut()">Sign out</a>
-                                </div>
-                            </li>
-                        </ul>
+                    <li>
+                        <img src="/images/ic-search.png" alt="">
+                        <input type="text" placeholder="Search by laboratory, people or location">
                     </li>
                 </ul>
             </div>
+
         </nav>
     </header>
 </template>
