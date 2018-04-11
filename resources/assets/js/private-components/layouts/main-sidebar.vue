981d00fb815d5a75418f475a0b230370966bff20
sergezak <sergezak9@gmail.com>
2018-04-11 20:35:28 +0300

    started to implement filter section in sidebar #4

--- resources/assets/js/private-components/layouts/main-sidebar.vue
+++ resources/assets/js/private-components/layouts/main-sidebar.vue
@@ -2,26 +2,39 @@
     <aside class="main-sidebar">
         <!-- sidebar: style can be found in sidebar.less -->
         <section class="sidebar">
-            <!-- Sidebar user panel -->
-            <div class="user-panel">
-                <div class="pull-left image">
-                    <img :src="user.avatar" class="img-circle" alt="User Image">
-                </div>
-                <div class="pull-left info">
-                    <p>Alexander Pierce</p>
-                    <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+
+            <div class="sidebar-form">
+                <div class="form-group">
+                    <select class="form-control select-filter type-filter">
+                        <option>Type</option>
+                        <option>option 2</option>
+                        <option>option 3</option>
+                        <option>option 4</option>
+                        <option>option 5</option>
+                    </select>
+                    <select class="form-control select-filter used-products-filter">
+                        <option>Used Products</option>
+                        <option>Tags</option>
+                        <option>option 3</option>
+                        <option>option 4</option>
+                        <option>option 5</option>
+                    </select>
+                    <select class="form-control select-filter tags-filter">
+                        <option>Tags</option>
+                        <option>option 2</option>
+                        <option>option 3</option>
+                        <option>option 4</option>
+                        <option>option 5</option>
+                    </select>
+                    <select class="form-control select-filter sort-by-filter">
+                        <option>Sort By</option>
+                        <option>option 2</option>
+                        <option>option 3</option>
+                        <option>option 4</option>
+                        <option>option 5</option>
+                    </select>
                 </div>
             </div>
-            <!-- search form -->
-            <form action="#" method="get" class="sidebar-form">
-                <div class="input-group">
-                    <input type="text" name="q" class="form-control" placeholder="Search...">
-                    <span class="input-group-btn">
-                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-                </div>
-            </form>
             <!-- /.search form -->
             <!-- sidebar menu: : style can be found in sidebar.less -->
             <ul class="sidebar-menu" data-widget="tree">
