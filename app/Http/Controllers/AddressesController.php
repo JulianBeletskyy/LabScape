8f9b9d5653c8574a6606033dbefe68ce46dc1cc9
sergezak <sergezak9@gmail.com>
2018-04-18 18:35:08 +0300

    applied sql queries at AddressesController@getContactsChain method #29

--- app/Http/Controllers/AddressesController.php
+++ app/Http/Controllers/AddressesController.php
@@ -138,7 +138,77 @@ class AddressesController extends Controller
 
     function getContactsChain(Address $address)
     {
-        return response()->json('OK');
+
+        $mainLabId = $address->id;
+
+        $sql = "SELECT * from
+                (SELECT a2.id, a2.name, a2.cluster_id FROM rl_addresses a  
+                JOIN rl_address_people ap ON a.id = ap.address_id -- workers of main hospital
+                JOIN rl_address_connections ac ON ac.from_person_id = ap.person_id -- people who know people on main hospital
+                JOIN rl_address_people ap2 ON ap2.person_id = ac.to_person_id -- workplaces of people who know people on main hospital
+                JOIN rl_addresses a2 ON ap2.address_id = a2.id 
+                WHERE a.id = " . $mainLabId . " 
+                UNION 
+                SELECT a2.id, a2.name, a2.cluster_id FROM rl_addresses a  
+                JOIN rl_address_people ap ON a.id = ap.address_id -- workers of main hospital
+                JOIN rl_address_connections ac ON ac.to_person_id = ap.person_id -- people who know people on main hospital 
+                JOIN rl_address_people ap2 ON ap2.person_id = ac.from_person_id -- workplaces of people who know people on main hospital
+                JOIN rl_addresses a2 ON ap2.address_id = a2.id 
+                WHERE a.id = " . $mainLabId . "
+                UNION
+                SELECT a2.id, a2.name, a2.cluster_id FROM rl_addresses a JOIN rl_addresses a2 ON a.cluster_id = a2.cluster_id WHERE a.id != a2.id AND a.cluster_id IS NOT NULL AND a.id = " . $mainLabId . " 
+                UNION 
+                SELECT a.id, a.name, a.cluster_id FROM rl_addresses a WHERE a.id = " . $mainLabId . ") related_labs ";
+
+        $related_labs = DB::select(DB::raw($sql));
+
+        $related_labs_ids = "";
+        $first = true;
+        foreach ($related_labs as $lab){
+            if ($first){
+                $first = false;
+            }else{
+                $related_labs_ids = $related_labs_ids . ",";
+            }
+            $related_labs_ids = $related_labs_ids . $lab ->id;
+        }
+
+        $sql = "SELECT rl.id, rl.name, ap.address_id, pt.name as 'workerType' FROM rl_people rl  
+                JOIN rl_address_people ap ON ap.person_id = rl.id JOIN rl_people_types pt ON rl.type_id = pt.id  
+                WHERE ap.address_id IN (" . $related_labs_ids . ")";
+
+        $lab_workers = DB::select(DB::raw($sql));
+
+
+        $related_people = [];
+        if ($related_labs_ids != ""){
+            $sql = "SELECT p.id, p.name, ap.address_id FROM rl_address_people ap JOIN rl_people p ON ap.person_id = p.id  WHERE ap.address_id IN (" . $related_labs_ids . ")";
+
+            $related_people = DB::select(DB::raw($sql));
+        }
+
+        // get the relations from related people
+        $first = true;
+        $related_people_ids = "";
+        foreach ($related_people as $p){
+            if ($first){
+                $first = false;
+            }else{
+                $related_people_ids = $related_people_ids . ",";
+            }
+            $related_people_ids = $related_people_ids . $p->id;
+        }
+
+        // get relationships and descriptions
+        $people_relationships = [];
+        if ($related_people_ids != ""){
+            $sql = "SELECT ac.from_person_id, ac.to_person_id, ac.edge_weight, act.id as 'connection_type' FROM rl_address_connections ac LEFT JOIN rl_address_connection_types act on ac.edge_type = act.id WHERE ac.from_person_id IN (" . $related_people_ids . ") AND ac.to_person_id IN (" . $related_people_ids . ") ";
+            $people_relationships = DB::select(DB::raw($sql));
+        }
+
+        $result = [ 'related_labs' => $related_labs, 'related_people' => $related_people, 'relationships' => $people_relationships, 'workers' => $lab_workers ];
+
+        return response()->json($result);
     }
 
 }
