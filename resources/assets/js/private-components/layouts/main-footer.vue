56926671ae8b983fe772aa1e260552cb1ae6bec5
sergezak <sergezak9@gmail.com>
2018-04-19 16:26:24 +0300

    fixed footer #28

--- resources/assets/js/private-components/layouts/main-footer.vue
+++ resources/assets/js/private-components/layouts/main-footer.vue
@@ -3,14 +3,18 @@
         <div class="pull-right hidden-xs">
             <b>Version</b> 2.4.0
         </div>
-        <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+        <strong>Copyright  {{currentYear}} PeakData GmbH.</strong> All rights
         reserved.
     </footer>
 </template>
 
 <script>
     export default {
-        name: "main-footer"
+        computed: {
+            currentYear: function () {
+                return moment().format('YYYY');
+            }
+        }
     }
 </script>
 
